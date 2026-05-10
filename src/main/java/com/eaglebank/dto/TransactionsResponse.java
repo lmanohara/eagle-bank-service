47a7a4ac2f51655e91a84ab2faa3e2fe63e9a7b7
lmanohara <lmanohara99@gmail.com>
2026-05-10 13:02:34 +0100

    fix: google java formatting

--- src/main/java/com/eaglebank/dto/TransactionsResponse.java
+++ src/main/java/com/eaglebank/dto/TransactionsResponse.java
@@ -1,12 +1,11 @@
 package com.eaglebank.dto;
 
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.List;
 
 @Getter
 @Setter
